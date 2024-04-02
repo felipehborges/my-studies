289066a64da91461f99e1adab0ac9e66ac9dfacc
Felipe Hamasaki <felipehama@gmail.com>
2024-04-02 16:58:32 -0300

    First commit

---
+++ nextjs-studies/rocketseat-next13-fundamentals/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <h1>hello-world</h1>
+      <p className="flex h-10 w-12 flex-col justify-normal">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia aut,
+        rerum veniam ea quasi, praesentium quidem numquam autem aperiam dolor
+        dolorem saepe magni minus unde deserunt, necessitatibus possimus
+        cupiditate consectetur?
+      </p>
+    </>
+  )
+}
