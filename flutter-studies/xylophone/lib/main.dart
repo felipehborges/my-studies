5e3b8f0fd9b5ea5b437d5d91887abe7ad35ec595
Felipe Hamasaki <felipehama@gmail.com>
2024-06-19 16:02:59 -0300

    Xylophone App

---
+++ flutter-studies/xylophone/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
